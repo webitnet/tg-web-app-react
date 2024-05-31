ed2fb4693dbd42fe314049637af098cd1dbded31
Andrii Hrytsiuk <andriihrytsiuk@Andriis-MacBook-Air.local>
2024-05-31 20:14:29 +0300

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
@@ -22,6 +23,30 @@ function App() {
             </Routes>
         </div>
     );
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+>>>>>>> 2dfcb9c (Initialize project using Create React App)
 }
 
 export default App;
