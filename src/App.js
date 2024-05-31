facfea4272a40d49834f3635a12f7e561526f9f6
Andrii Hrytsiuk <andriihrytsiuk@Andriis-MacBook-Air.local>
2024-05-31 21:43:17 +0300

    start commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-<<<<<<< HEAD
 import './App.css';
-import {useEffect} from "react";
-import {useTelegram} from "./hooks/useTelegram";
+import { useEffect } from "react";
+import { useTelegram } from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
-import {Route, Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
 
 function App() {
-    const {onToggleButton, tg} = useTelegram();
+    const { onToggleButton, tg } = useTelegram();
 
     useEffect(() => {
         tg.ready();
@@ -18,35 +17,11 @@ function App() {
         <div className="App">
             <Header />
             <Routes>
-                <Route index element={<ProductList />}/>
-                <Route path={'form'} element={<Form />}/>
+                <Route index element={<ProductList />} />
+                <Route path={'form'} element={<Form />} />
             </Routes>
         </div>
     );
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
->>>>>>> 2dfcb9c (Initialize project using Create React App)
 }
 
 export default App;
